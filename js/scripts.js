22d603adc00c3a0a2b48a32f61cb0284d940bd3b
Jeff Dean <deanjef2@hotmail.com>
2016-02-20 15:36:59 -0800

    begins js but does not work yet

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $("form#questions").submit(function(event) {
+
+    if ("input#hot" === yes) {
+      $("#Hawaii").show();
+    }
+
+    if ("input#hot" === yes) {
+      $("#Hawaii").show();
+    }
+
+    if ("input#hot" === yes) {
+      $("#Hawaii").show();
+    }
+
+    if ("input#hot" === yes) {
+      $("#Hawaii").show();
+    }
+
+    if ("input#hot" === yes) {
+      $("#Hawaii").show();
+    }
+
+    event.preventDefault();
+  });
+});
