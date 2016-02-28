eb585f791dfa58ac5375d7203e10d32ab526d036
Jeff Dean <deanjef2@hotmail.com>
2016-02-28 03:57:59 -0800

    adjusts scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,21 @@
 $(document).ready(function() {
   $("form#questions").submit(function(event) {
+    var hot = $("select#hot").val();
+    var cold = $("select#cold").val();
+    var animals = $("select#animals").val();
+    var swimming = $("select#swimming").val();
+    var age = $("select#age").val();
 
-    if ("input#hot" === yes) {
-      $("#Hawaii").show();
+    if (age === "ageYes" && swimming === "swimmingNo") {
+      $("#Rome").show();
+    } else if (age === "ageNo" & swimming === "swimmingYes")
+      $("#Antarctica").show();
     }
 
-    if ("input#hot" === yes) {
+    if (animals === "animalsYes" && hot === "hotYes")
       $("#Hawaii").show();
-    }
-
-    if ("input#hot" === yes) {
-      $("#Hawaii").show();
-    }
-
-    if ("input#hot" === yes) {
-      $("#Hawaii").show();
-    }
-
-    if ("input#hot" === yes) {
-      $("#Hawaii").show();
-    }
-
+    } else if (animals === "animalsNo" && hot === "hotNo")
+      $("#Rome").show();
     event.preventDefault();
   });
 });
